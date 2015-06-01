749d4a2ed51cc394fdd8b1c00f9ed432642fab6b
imningfeng <imningfeng@gmail.com>
2015-06-01 21:50:12 +0800

    添加部署文件

---
+++ system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'hzq',
+  'password' => 'Nf18280257530',
+  'dbname' => 'hzq',
+);
+$config['slave'] = false;
